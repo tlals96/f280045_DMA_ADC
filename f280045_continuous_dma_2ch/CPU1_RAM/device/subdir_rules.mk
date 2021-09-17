ccc8e6312cf18e9b223d528cf223aed7f2e2ea34
tlals96 <tlals96@gmail.com>
2021-09-17 16:32:17 +0900

    DMA ADC Rev0.1
    f280045 dma adc file upload

---
+++ f280045_continuous_dma_2ch/CPU1_RAM/device/subdir_rules.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+device/%.obj: ../device/%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --tmu_support=tmu0 --vcu_support=vcu0 -Ooff --include_path="C:/ccs_workspace/Projects/f280045_continuous_dma_2ch_20210913" --include_path="C:/ccs_workspace/Projects/f280045_continuous_dma_2ch_20210913/device" --include_path="C:/ti/c2000/C2000Ware_3_04_00_00/driverlib/f28004x/driverlib" --include_path="C:/ti/c2000/C2000Ware_3_04_00_00/libraries/calibration/hrpwm/f28004x/include" --include_path="C:/ti/include" --define=DEBUG --define=CPU1 --diag_suppress=10063 --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="device/$(basename $(<F)).d_raw" --include_path="C:/ccs_workspace/Projects/f280045_continuous_dma_2ch_20210913/CPU1_RAM/syscfg" --obj_directory="device" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+device/%.obj: ../device/%.asm $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --tmu_support=tmu0 --vcu_support=vcu0 -Ooff --include_path="C:/ccs_workspace/Projects/f280045_continuous_dma_2ch_20210913" --include_path="C:/ccs_workspace/Projects/f280045_continuous_dma_2ch_20210913/device" --include_path="C:/ti/c2000/C2000Ware_3_04_00_00/driverlib/f28004x/driverlib" --include_path="C:/ti/c2000/C2000Ware_3_04_00_00/libraries/calibration/hrpwm/f28004x/include" --include_path="C:/ti/include" --define=DEBUG --define=CPU1 --diag_suppress=10063 --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="device/$(basename $(<F)).d_raw" --include_path="C:/ccs_workspace/Projects/f280045_continuous_dma_2ch_20210913/CPU1_RAM/syscfg" --obj_directory="device" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
---
+++ f280045_continuous_dma_2ch/CPU1_RAM/subdir_vars.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../28004x_generic_ram_lnk.cmd 
+
+LIB_SRCS += \
+C:/ti/c2000/C2000Ware_3_04_00_00/libraries/calibration/hrpwm/f28004x/lib/SFO_v8_fpu_lib_build_c28_driverlib.lib \
+C:/ti/c2000/C2000Ware_3_04_00_00/driverlib/f28004x/driverlib/ccs/Debug/driverlib.lib 
+
+C_SRCS += \
+../adc_ex6_soc_continuous_dma.c 
+
+C_DEPS += \
+./adc_ex6_soc_continuous_dma.d 
+
+OBJS += \
+./adc_ex6_soc_continuous_dma.obj 
+
+OBJS__QUOTED += \
+"adc_ex6_soc_continuous_dma.obj" 
+
+C_DEPS__QUOTED += \
+"adc_ex6_soc_continuous_dma.d" 
+
+C_SRCS__QUOTED += \
+"../adc_ex6_soc_continuous_dma.c" 
+
+
